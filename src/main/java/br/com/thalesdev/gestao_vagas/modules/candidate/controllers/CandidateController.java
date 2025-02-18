df8dbdea6b18ba027cd06ebd4a23de49bb10da7f
thales-sblue <thales_sblue@hotmail.com>
2025-02-18 16:37:16 -0300

    chore: setup project structure

---
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,19 @@
+package br.com.thalesdev.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    }    
+
+}
