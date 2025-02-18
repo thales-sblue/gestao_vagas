aa631b89375335fed50efc75cf4e0755376dfa7a
thales-sblue <thales_sblue@hotmail.com>
2025-02-18 16:50:35 -0300

    feat: add validation constraints to CandidateEntity

--- src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,6 +2,9 @@ package br.com.thalesdev.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }    
--- src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.thalesdev.gestao_vagas.modules.candidate.controllers;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "O campo (username) deve conter apenas letras e numeros")
     private String username;
+
+    @Email(message = "O campo (email) deve conter um e-mail valido")
     private String email;
+
+    @Length(min = 6, message = "O campo (password) deve conter no minimo 6 caracteres") 
     private String password;
     private String description;
     private String curriculum;
