fb4e6188ce70f03a2c7cf323675005564b070352
thales-sblue <thales_sblue@hotmail.com>
2025-03-06 15:02:43 -0300

    refactor(controller): refactor JobController

--- src/main/java/br/com/thalesdev/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,12 +1,16 @@
 package br.com.thalesdev.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.thalesdev.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.thalesdev.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.thalesdev.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,9 +23,17 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
     
     @PostMapping("/")
-    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
-        return this.createJobUseCase.execute(jobEntity);
+    public JobEntity create(@Valid @RequestBody CreateJobDTO CreateJobDTO, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_id");
 
+        var jobEntity = JobEntity.builder()
+                .description(CreateJobDTO.getDescription())
+                .benefits(CreateJobDTO.getBenefits())
+                .level(CreateJobDTO.getLevel())
+                .companyId(UUID.fromString(companyId.toString()))
+                .build();
+
+        return this.createJobUseCase.execute(jobEntity);
     }
     
 }
