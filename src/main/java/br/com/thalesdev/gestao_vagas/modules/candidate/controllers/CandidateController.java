d950aae4e576a4b3a3882ad1eddc63ff88a965dc
thales-sblue <thales_sblue@hotmail.com>
2025-03-07 15:54:17 -0300

    refactor: update AuthCandidate controller

--- src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.thalesdev.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.thalesdev.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.thalesdev.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
+import java.util.UUID;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -20,6 +29,9 @@ public class CandidateController {
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
 
+    @Autowired
+    ProfileCandidateUseCase profileCandidateUseCase;
+
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
@@ -27,8 +39,20 @@ public class CandidateController {
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
-        }
-        
+        }        
     }    
+
+    @GetMapping("/")
+    public ResponseEntity<Object> get(HttpServletRequest request) {
+
+        var idCandidate = request.getAttribute("candidate_id");
+        try {
+            var profile = this.profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
+            return ResponseEntity.ok().body(profile);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }  
+    }
+    
 
 }
---
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,61 @@
+package br.com.thalesdev.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.thalesdev.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.thalesdev.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.thalesdev.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException{ 
+        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+            .orElseThrow(() -> {
+                throw new UsernameNotFoundException("Username/password incorrect");
+                });
+
+        if(!passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword())){
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+        var token = JWT.create()
+            .withIssuer("javagas")
+            .withSubject(candidate.getId().toString())
+            .withClaim("roles", Arrays.asList("candidate"))
+            .withExpiresAt(expiresIn)
+            .sign(algorithm);
+        
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+            .acess_token(token)
+            .expires_in(expiresIn.toEpochMilli())
+            .build();
+
+        return authCandidateResponse;
+    }
+    
+}
