6b1136344fa51713991f39afd263ece6d153f245
thales-sblue <thales_sblue@hotmail.com>
2025-03-17 20:58:41 -0300

    feat: add FirstTest

---
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,12 @@
+package br.com.thalesdev.gestao_vagas.modules.candidate.useCases;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+    public void execute() {
+
+    }
+
+}
