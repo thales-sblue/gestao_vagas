8b8490a5836f836cf3475346e75ccee97fc9f3a6
thales-sblue <thales_sblue@hotmail.com>
2025-03-17 21:28:45 -0300

    test(candidate): add test for candidate not found scenario in Job application

---
+++ src/test/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,36 @@
+package br.com.thalesdev.gestao_vagas.modules.candidate.useCases;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.thalesdev.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.thalesdev.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.thalesdev.gestao_vagas.modules.company.repositories.JobRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("should not be able to apply job with candidate not found")
+    public void should_not_be_able_to_apply_job_with_candidate_not_found() {
+        try {
+            applyJobCandidateUseCase.execute(null, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+}
