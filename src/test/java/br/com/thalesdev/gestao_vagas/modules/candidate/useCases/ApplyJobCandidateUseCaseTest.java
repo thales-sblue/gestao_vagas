0481b3b64b318b2cf781210b1e499ff02caa1d75
thales-sblue <thales_sblue@hotmail.com>
2025-03-18 20:52:58 -0300

    test(apply-job) add validation for missing job in application

--- src/test/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -7,10 +7,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.thalesdev.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.thalesdev.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.thalesdev.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.thalesdev.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.thalesdev.gestao_vagas.modules.company.repositories.JobRepository;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplyJobCandidateUseCaseTest {
@@ -31,6 +37,23 @@ public class ApplyJobCandidateUseCaseTest {
             applyJobCandidateUseCase.execute(null, null);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("should not be able to apply job with job not found")
+    public void should_not_be_able_to_apply_job_with_job_not_found() {
+        var candidateId = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+        candidate.setId(candidateId);
+
+        when(candidateRepository.findById(candidateId)).thenReturn(Optional.of(candidate));
+
+        try {
+            applyJobCandidateUseCase.execute(candidateId, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
         }
     }
 }
