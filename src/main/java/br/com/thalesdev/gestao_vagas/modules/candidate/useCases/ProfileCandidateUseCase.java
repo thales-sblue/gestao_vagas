f8fb9a60f6da1e2201ab716149f23ad035df9e9a
thales-sblue <thales_sblue@hotmail.com>
2025-03-25 22:29:36 -0300

    chore: add SonarQube integration for code quality analysis

--- src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -3,9 +3,9 @@ package br.com.thalesdev.gestao_vagas.modules.candidate.useCases;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.com.thalesdev.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.thalesdev.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.thalesdev.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 
@@ -17,19 +17,19 @@ public class ProfileCandidateUseCase {
 
     public ProfileCandidateResponseDTO execute(UUID idCandidate) {
         var candidate = this.candidateRepository.findById(idCandidate)
-            .orElseThrow(() -> {
-                    throw new UsernameNotFoundException("User not found.");
+                .orElseThrow(() -> {
+                    throw new UserNotFoundException();
                 });
 
         var candidateDto = ProfileCandidateResponseDTO.builder()
-            .description(candidate.getDescription())
-            .email(candidate.getEmail())
-            .id(candidate.getId())
-            .name(candidate.getName())
-            .username(candidate.getUsername())
-            .build();
+                .description(candidate.getDescription())
+                .email(candidate.getEmail())
+                .id(candidate.getId())
+                .name(candidate.getName())
+                .username(candidate.getUsername())
+                .build();
 
-        return candidateDto;            
+        return candidateDto;
     }
-    
+
 }
--- src/test/java/br/com/thalesdev/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/thalesdev/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -17,13 +17,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import br.com.thalesdev.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.thalesdev.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.thalesdev.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.thalesdev.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.thalesdev.gestao_vagas.utils.TestUtils;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
