98a6f551897665005f3ca8fcdb6ac42c1dd87293
thales-sblue <thales_sblue@hotmail.com>
2025-04-16 01:23:49 -0300

    feat: add authcandidate roles

--- src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/br/com/thalesdev/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -31,31 +31,35 @@ public class AuthCandidateUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException{ 
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+            throws AuthenticationException {
         var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
-            .orElseThrow(() -> {
-                throw new UsernameNotFoundException("Username/password incorrect");
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("Username/password incorrect");
                 });
 
-        if(!passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword())){
+        if (!passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword())) {
             throw new AuthenticationException();
         }
+
+        var roles = Arrays.asList("CANDIDATE");
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         var expiresIn = Instant.now().plus(Duration.ofHours(2));
         var token = JWT.create()
-            .withIssuer("javagas")
-            .withSubject(candidate.getId().toString())
-            .withClaim("roles", Arrays.asList("CANDIDATE"))
-            .withExpiresAt(expiresIn)
-            .sign(algorithm);
-        
+                .withIssuer("javagas")
+                .withSubject(candidate.getId().toString())
+                .withClaim("roles", roles)
+                .withExpiresAt(expiresIn)
+                .sign(algorithm);
+
         var authCandidateResponse = AuthCandidateResponseDTO.builder()
-            .access_token(token)
-            .expires_in(expiresIn.toEpochMilli())
-            .build();
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .roles(roles)
+                .build();
 
         return authCandidateResponse;
     }
-    
+
 }
